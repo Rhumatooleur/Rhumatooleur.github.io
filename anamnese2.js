351e3fbbe31b1d2ce1aac299a05b8ac6123254d5
Rhumatooleur <quentincampeol@gmail.com>
2024-07-04 11:29:51 +0200

    améliorations

--- anamnese2.js
+++ anamnese2.js
@@ -43,10 +43,12 @@ function submitForm() {
             syndromeSec: document.getElementById('syndromeSec').checked,
             xerostomie: document.getElementById('xerostomie').checked,
             aphtoseBipolaire: document.getElementById('aphtoseBipolaire').checked,
+            commentaires: document.getElementById('commentSymptomatologieExtraArticulaire').value.trim(),
         },
         antecedentsFamiliaux: {
             polyarthriteRhumatoide: document.getElementById('polyarthriteRhumatoide').checked,
             spondyloarthrite: document.getElementById('spondyloarthrite').checked,
+            commentaires: document.getElementById('commentAntecedentsFamiliaux').value.trim(),
         },
         surLePlanGeneral: {
             fievre: document.getElementById('fievre').checked,
@@ -54,6 +56,7 @@ function submitForm() {
             asthenie: document.getElementById('asthenie').checked,
             anorexie: document.getElementById('anorexie').checked,
             pertePoids: document.getElementById('pertePoids').checked,
+            commentaires: document.getElementById('commentSurLePlanGeneral').value.trim(),
         },
         examensComplementaires: document.getElementById('examensComplementaires').value.trim(),
         therapeutiques: document.getElementById('therapeutiques').value.trim(),
@@ -63,134 +66,240 @@ function submitForm() {
             nue: document.getElementById('nue').checked,
             associeeA: document.getElementById('associeeA').value.trim(),
             evolution: document.querySelector('input[name="evolution"]:checked') ? document.querySelector('input[name="evolution"]:checked').value : '',
+            commentaires: document.getElementById('commentAuTotal').value.trim(),
         }
     };
 
     let resultText = "";
 
-    // Construction du texte à afficher
+    // Symptomatologie articulaire
     resultText += "Symptomatologie articulaire :\n";
     resultText += "- Nb articulations gonflées (synovites) :\n";
     resultText += generateArticulationsText(formData.symptomatologieArticulaire.synovites);
-    resultText += "\n";
-    resultText += "- Nb articulations douloureuses :\n";
+    resultText += "\n- Nb articulations douloureuses :\n";
     resultText += generateArticulationsText(formData.symptomatologieArticulaire.douleurs);
-    resultText += "\n";
+    resultText += "\n- Horaire des douleurs : ";
+    resultText += formData.symptomatologieArticulaire.horaireDouleurs === '' ? "non spécifié" : formData.symptomatologieArticulaire.horaireDouleurs;
+    resultText += "\n- L'évolution se fait par : ";
+    resultText += (formData.symptomatologieArticulaire.evolution.poussee ? "poussée" : "") + (formData.symptomatologieArticulaire.evolution.poussee && formData.symptomatologieArticulaire.evolution.aggravation ? ", " : "") + (formData.symptomatologieArticulaire.evolution.aggravation ? "aggravation progressive" : "");
+    resultText += "\n\n";
 
-    resultText += "- Horaire des douleurs : " + formData.symptomatologieArticulaire.horaireDouleurs + "\n";
-    if (formData.symptomatologieArticulaire.evolution.poussee || formData.symptomatologieArticulaire.evolution.aggravation) {
-        resultText += "- L'évolution se fait par : ";
-        let evolutions = [];
-        if (formData.symptomatologieArticulaire.evolution.poussee) {
-            evolutions.push("poussée");
-        }
-        if (formData.symptomatologieArticulaire.evolution.aggravation) {
-            evolutions.push("aggravation progressive");
-        }
-        resultText += evolutions.join(", ") + "\n";
-    }
-    resultText += "\n";
-
+    // Symptomatologie extra-articulaire
     resultText += "Symptomatologie extra-articulaire :\n";
     resultText += generateSymptomatologieExtraArticulaireText(formData.symptomatologieExtraArticulaire);
     resultText += "\n";
 
+    // Antécédents familiaux
     resultText += "Antécédents familiaux :\n";
     resultText += generateAntecedentsFamiliauxText(formData.antecedentsFamiliaux);
     resultText += "\n";
 
+    // Sur le plan général
     resultText += "Sur le plan général :\n";
     resultText += generateSurLePlanGeneralText(formData.surLePlanGeneral);
     resultText += "\n";
 
-    if (formData.examensComplementaires) {
-        resultText += "Examens complémentaires réalisés :\n" + formData.examensComplementaires + "\n\n";
-    }
+    // Examens complémentaires
+    resultText += "Examens complémentaires : ";
+    resultText += formData.examensComplementaires === '' ? "non spécifié" : formData.examensComplementaires;
+    resultText += "\n\n";
 
-    if (formData.therapeutiques) {
-        resultText += "Thérapeutiques :\n" + formData.therapeutiques + "\n\n";
-    }
+    // Thérapeutiques
+    resultText += "Thérapeutiques : ";
+    resultText += formData.therapeutiques === '' ? "non spécifié" : formData.therapeutiques;
+    resultText += "\n\n";
 
+    // Au total
     resultText += "Au total :\n";
+    resultText += "- Il s'agit d'une ";
     let totalConditions = [];
     if (formData.auTotal.polyarthrite) {
-        totalConditions.push("Polyarthrite");
+        totalConditions.push("polyarthrite");
     }
     if (formData.auTotal.polyarthralgies) {
-        totalConditions.push("Polyarthralgies");
+        totalConditions.push("polyarthralgies");
     }
     if (formData.auTotal.nue) {
         totalConditions.push("nue");
     }
-    if (formData.auTotal.associeeA) {
-        totalConditions.push("associée à " + formData.auTotal.associeeA);
-    }
-    if (formData.auTotal.evolution) {
-        totalConditions.push("évoluant depuis " + (formData.auTotal.evolution === "plus6semaines" ? "plus de 6 semaines" : "moins de 6 semaines"));
-    }
-    resultText += totalConditions.join(", ");
-    if (totalConditions.length > 0) {
-        resultText += "\n";
+    resultText += totalConditions.join(" ou ") + " selon la case cochée\n";
+    resultText += "- Associée à : ";
+    resultText += formData.auTotal.associeeA === '' ? "non spécifié" : formData.auTotal.associeeA;
+    resultText += "\n- Évolution : ";
+    resultText += formData.auTotal.evolution === '' ? "non spécifié" : (formData.auTotal.evolution === "plus6semaines" ? "plus de 6 semaines" : "moins de 6 semaines");
+    resultText += "\n";
+    if (formData.auTotal.commentaires !== '') {
+        resultText += formData.auTotal.commentaires + "\n\n";
     } else {
-        resultText += "Non spécifié\n";
+        resultText += "\n";
     }
 
-    // Afficher le résultat dans la textarea dédiée
-    const resultTextarea = document.getElementById('resultat');
-    resultTextarea.value = resultText;
+    // Affichage du résultat
+    document.getElementById('resultat').value = resultText;
+
+    // Copier le résultat dans le presse-papier
+    copyToClipboard(resultText);
 }
 
-// Fonction pour générer le texte des articulations
-function generateArticulationsText(data) {
-    let articulations = [];
-    if (data.IPPs) articulations.push("IPPs");
-    if (data.MCPs) articulations.push("MCPs");
-    if (data.Poignets) articulations.push("Poignets");
-    if (data.Coudes) articulations.push("Coudes");
-    if (data.Epaules) articulations.push("Epaules");
-    if (data.Genoux) articulations.push("Genoux");
-    if (data.Autres) articulations.push("Autres : " + data.Autres);
-    return articulations.join(", ");
+function generateArticulationsText(articulations) {
+    let text = "";
+    if (articulations.IPPs) {
+        text += "IPPs, ";
+    }
+    if (articulations.MCPs) {
+        text += "MCPs, ";
+    }
+    if (articulations.Poignets) {
+        text += "Poignets, ";
+    }
+    if (articulations.Coudes) {
+        text += "Coudes, ";
+    }
+    if (articulations.Epaules) {
+        text += "Epaules, ";
+    }
+    if (articulations.Genoux) {
+        text += "Genoux, ";
+    }
+    if (articulations.Autres !== '') {
+        text += "Autres : " + articulations.Autres;
+    }
+    // Enlever la dernière virgule et l'espace si elle existe
+    if (text.endsWith(", ")) {
+        text = text.slice(0, -2);
+    }
+    return text === "" ? "non spécifié" : text;
 }
 
-// Fonction pour générer le texte de la symptomatologie extra-articulaire
-function generateSymptomatologieExtraArticulaireText(data) {
-    let symptoms = [];
-    if (data.douleursAbdominales) symptoms.push("douleurs abdominales");
-    if (data.diarrhee) symptoms.push("diarrhée");
-    if (data.bruluresMictionnelles) symptoms.push("brûlures mictionnelles");
-    if (data.ecoulementsUretraux) symptoms.push("écoulements urétraux");
-    if (data.conjonctivite) symptoms.push("conjonctivite");
-    if (data.angine) symptoms.push("angine");
-    if (data.raynaud) symptoms.push("phénomène de Raynaud");
-    if (data.psoriasis) symptoms.push("psoriasis");
-    if (data.vascularites) symptoms.push("vascularites");
-    if (data.exhantheme) symptoms.push("exanthème");
-    if (data.eruptionCutanee) symptoms.push("éruption cutanée photosensible");
-    if (data.erythemeNoueux) symptoms.push("érythème noueux");
-    if (data.acneeSevere) symptoms.push("acné sévère");
-    if (data.crisesFebriles) symptoms.push("crises fébriles");
-    if (data.syndromeSec) symptoms.push("syndrome sec");
-    if (data.xerostomie) symptoms.push("xérostomie");
-    if (data.aphtoseBipolaire) symptoms.push("aphtose bipolaire");
-    return symptoms.join(", ");
+function generateSymptomatologieExtraArticulaireText(extraArticulaire) {
+    let text = "";
+    if (extraArticulaire.douleursAbdominales) {
+        text += "douleurs abdominales, ";
+    }
+    if (extraArticulaire.diarrhee) {
+        text += "diarrhée, ";
+    }
+    if (extraArticulaire.bruluresMictionnelles) {
+        text += "brûlures mictionnelles, ";
+    }
+    if (extraArticulaire.ecoulementsUretraux) {
+        text += "écoulements urétraux, ";
+    }
+    if (extraArticulaire.conjonctivite) {
+        text += "conjonctivite, ";
+    }
+    if (extraArticulaire.angine) {
+        text += "angine, ";
+    }
+    if (extraArticulaire.raynaud) {
+        text += "phénomène de Raynaud, ";
+    }
+    if (extraArticulaire.psoriasis) {
+        text += "psoriasis, ";
+    }
+    if (extraArticulaire.vascularites) {
+        text += "vascularites, ";
+    }
+    if (extraArticulaire.exhantheme) {
+        text += "exanthème, ";
+    }
+    if (extraArticulaire.eruptionCutanee) {
+        text += "éruption cutanée photosensible, ";
+    }
+    if (extraArticulaire.erythemeNoueux) {
+        text += "érythème noueux, ";
+    }
+    if (extraArticulaire.acneeSevere) {
+        text += "acné sévère et pustulose palmo-plantaire (sapho), ";
+    }
+    if (extraArticulaire.crisesFebriles) {
+        text += "crises fébriles + douleurs abdo (MEFV), ";
+    }
+    if (extraArticulaire.syndromeSec) {
+        text += "syndrome sec oculaire, ";
+    }
+    if (extraArticulaire.xerostomie) {
+        text += "xérostomie, ";
+    }
+    if (extraArticulaire.aphtoseBipolaire) {
+        text += "aphtose bipolaire, ";
+    }
+    if (extraArticulaire.commentaires !== '') {
+        text += extraArticulaire.commentaires;
+    }
+    // Enlever la dernière virgule et l'espace si elle existe
+    if (text.endsWith(", ")) {
+        text = text.slice(0, -2);
+    }
+    // Ajouter un point à la fin si aucun commentaire supplémentaire
+    if (!text.endsWith(":")) {
+        text += ".";
+    }
+    return text === "" ? "non spécifié" : text;
 }
 
-// Fonction pour générer le texte des antécédents familiaux
-function generateAntecedentsFamiliauxText(data) {
-    let antecedents = [];
-    if (data.polyarthriteRhumatoide) antecedents.push("polyarthrite rhumatoïde");
-    if (data.spondyloarthrite) antecedents.push("spondyloarthrite");
-    return antecedents.join(", ");
+function generateAntecedentsFamiliauxText(antecedents) {
+    let text = "";
+    if (antecedents.polyarthriteRhumatoide) {
+        text += "polyarthrite rhumatoïde, ";
+    }
+    if (antecedents.spondyloarthrite) {
+        text += "spondyloarthrite, ";
+    }
+    if (antecedents.commentaires !== '') {
+        text += antecedents.commentaires;
+    }
+    // Enlever la dernière virgule et l'espace si elle existe
+    if (text.endsWith(", ")) {
+        text = text.slice(0, -2);
+    }
+    // Ajouter un point à la fin si aucun commentaire supplémentaire
+    if (!text.endsWith(":")) {
+        text += ".";
+    }
+    return text === "" ? "non spécifié" : text;
 }
 
-// Fonction pour générer le texte sur le plan général
-function generateSurLePlanGeneralText(data) {
-    let generalPlan = [];
-    if (data.fievre) generalPlan.push("fièvre");
-    if (data.sueursNocurnes) generalPlan.push("sueurs nocturnes");
-    if (data.asthenie) generalPlan.push("asthénie");
-    if (data.anorexie) generalPlan.push("anorexie");
-    if (data.pertePoids) generalPlan.push("perte de poids");
-    return generalPlan.join(", ");
+function generateSurLePlanGeneralText(planGeneral) {
+    let text = "";
+    if (planGeneral.fievre) {
+        text += "fièvre, ";
+    }
+    if (planGeneral.sueursNocurnes) {
+        text += "sueurs nocturnes, ";
+    }
+    if (planGeneral.asthenie) {
+        text += "asthénie, ";
+    }
+    if (planGeneral.anorexie) {
+        text += "anorexie, ";
+    }
+    if (planGeneral.pertePoids) {
+        text += "perte de poids, ";
+    }
+    if (planGeneral.commentaires !== '') {
+        text += planGeneral.commentaires;
+    }
+    // Enlever la dernière virgule et l'espace si elle existe
+    if (text.endsWith(", ")) {
+        text = text.slice(0, -2);
+    }
+    // Ajouter un point à la fin si aucun commentaire supplémentaire
+    if (!text.endsWith(":")) {
+        text += ".";
+    }
+    return text === "" ? "non spécifié" : text;
+}
+
+function copyToClipboard(text) {
+    const textarea = document.createElement('textarea');
+    textarea.value = text;
+    textarea.setAttribute('readonly', '');
+    textarea.style.position = 'absolute';
+    textarea.style.left = '-9999px';
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+    alert('Résultat copié dans le presse-papiers.');
 }
