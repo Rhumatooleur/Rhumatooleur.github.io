c0a2c7d4cdbbaa0b9c433e51bf638a5f18e48636
Rhumatooleur <quentincampeol@gmail.com>
2024-11-11 20:23:45 +0100

    CHU 1.3

--- cmbCHU.js
+++ cmbCHU.js
@@ -15,10 +15,15 @@ $(function() {
       }
     });
   
-    var finalText = selectedTexts.join("\n\n");
+    var finalText = selectedTexts.join("\n");
     document.getElementById("finalText").value = finalText;
   });
   
+    // Fonction pour limiter les sauts de ligne consécutifs
+    function removeExtraNewlines(text) {
+        return text.replace(/\n{3,}/g, '\n'); // Remplacer 3 ou plus sauts de ligne consécutifs par 2 sauts de ligne
+    }
+
   document.getElementById("formatButton").addEventListener("click", function() {
     // Récupérer le texte entré par l'utilisateur
     var inputText = document.getElementById("inputText").value;
@@ -39,7 +44,7 @@ $(function() {
 
   
     // Créer une chaîne pour stocker le texte formaté de la bio de base
-    var bioDeBaseText = '';
+    var bioDeBaseText = 'Hémogramme : ';
   
     // Définition des paramètres de la bio de base et leurs noms formatés avec les unités
 var bioDeBaseParams = {
@@ -48,17 +53,18 @@ var bioDeBaseParams = {
     "Polynucléaires neutrophiles": { name: " PNNs", unit: "G/L,", variations: ["PNNs", "Polynucléaires neutrophiles", "Polynucléairesneutrophiles"] },
     "Plaquettes": { name: "Plaquettes", unit: "G/L", variations: ["Thrombocytes"] },
     "CRP": { name: "CRP", unit: "mg/L ", variations: ["ProtéineCréactive"] },
-    "Sodium": { name: "Na", unit: "mmol/L, ", variations: ["Sodium"] },
+    "Sodium": { name: "Ionogramme : Na", unit: "mmol/L, ", variations: ["Sodium"] },
     "Potassium": { name: " K", unit: "mmol/L, ", variations: ["Potassium"] },
     "Urée": { name: " Urée", unit: "mmol/L,", variations: ["Urée"] },
-    "Créatinine": { name: "Créatinine", unit: "µmol/L", variations: ["Créatinine"] },
-    "ASAT": { name: "ASAT", unit: "UI/L,  ", variations: ["ASAT-SGOT", "ASAT"] }, 
+    "Créatinine": { name: " Créatinine", unit: "µmol/L, ", variations: ["Créatinine"] },
+    "DFG": { name: "DFG (CKD-EPI)", unit: "mL/min", variations: ["CKD-EPI"] },
+    "ASAT": { name: "Bilan hépatique : ASAT", unit: "UI/L,  ", variations: ["ASAT-SGOT", "ASAT"] }, 
     "ALAT": { name: " ALAT", unit: "UI/L,  ", variations: ["ALAT-SGPT", "ALAT"] },
     "Phosphatases alcalines": { name: "PAL", unit: "UI/L,  ", variations: ["Phos.Alcalines", "Phosphatases alcalines", "Phosphatase alcaline", "Phosphatasealcaline"] },
     "Gamma-GT": { name: "GGT", unit: "UI/L, ", variations: ["Gamma GT", "Gamma-GT", "Gamma Glutamyl Transférase", "GammaGlutamylTransférase"] },
     "Bilirubine totale": { name: "Bilirubine totale", unit: "µmol/l", variations: ["Bilirubine totale", "Bilirubinetotale"] },
-    "TP": { name: "TP", unit: "%, ", variations: ["Tauxdeprothrombine"] },
-    "INR": { name: "INR", unit: ", ", variations: ["INR"] },
+    "TP": { name: "Bilan de coagulation : TP", unit: "%, ", variations: ["Tauxdeprothrombine"] },
+    "INR": { name: " INR", unit: ",", variations: ["INR"] },
     "TCA Patient/Témoin":{ name:"TCA ratio", unit: " ", variations: ["Ratiopatient/témoin"]}
 };
 
@@ -66,29 +72,31 @@ function findValue(variations, input, unit) {
     for (var i = 0; i < variations.length; i++) {
         var pattern;
         
-        // Cas spécifique pour "Polynucléaires neutrophiles" et "Polynucléairesneutrophiles"
-        if (variations[i] === "Polynucléaires neutrophiles" || variations[i] === "Polynucléairesneutrophiles") {
-            // Chercher le pourcentage suivi de la valeur en "G/l"
+        if (variations[i] === "INR") {
+            // Chercher la valeur de l'INR
+            pattern = new RegExp(variations[i] + "\\s*[:\\s]*\\s*(<|>)?\\s*([\\d.,]+)", "i");
+            var match = input.match(pattern);
+            if (match) {
+                // Conserver uniquement les 4 premiers caractères après la virgule
+                return { operator: match[1] || "", value: match[2].substring(0, 4), unit: unit };
+            }
+        } else if (variations[i] === "Polynucléaires neutrophiles" || variations[i] === "Polynucléairesneutrophiles") {
             pattern = new RegExp(variations[i] + "\\s*[:\\s]*\\s*(?:B|H)?\\s*(<|>)?\\s*\\d+(?:,\\d+)?%\\s*(\\d+(?:,\\d+)?)\\s*G/l", "i");
         } else {
-            // Pour les autres paramètres, continuer la recherche classique
             pattern = new RegExp(variations[i] + "\\s*[:\\s]*\\s*(?:B|H)?\\s*(<|>)?\\s*([\\d.,]+)\\s*(" + unit + ")?", "i");
         }
 
         var match = input.match(pattern);
         if (match) {
             if (variations[i] === "Polynucléaires neutrophiles" || variations[i] === "Polynucléairesneutrophiles") {
-                // Si on a trouvé le pourcentage, retourner la valeur en "G/l" après le pourcentage
                 return { operator: match[1] || "", value: match[2].replace(",", "."), unit: "G/l" };
             } else {
-                // Sinon, retourner la valeur classique trouvée
                 return { operator: match[1] || "", value: match[2].replace(",", "."), unit: match[3] || unit };
             }
         }
     }
     return null;
 }
-
 
 // Remplacer les valeurs dans le texte formaté de la bio de base et les placer à leurs positions respectives
 
@@ -98,7 +106,7 @@ for (var param in bioDeBaseParams) {
     var valueObject = findValue(bioDeBaseParams[param].variations, preprocessedText, bioDeBaseParams[param].unit);
     if (valueObject) {
         var formattedValue = valueObject.operator + " " + valueObject.value + " " + (bioDeBaseParams[param].unit === "%" ? "" : bioDeBaseParams[param].unit);
-        if (param === "Hémoglobine" || param === "ASAT" || param === "ALAT" || param === "Phosphatases alcalines" || param === "Gamma-GT" || param === "TP" || param === "INR" || param === "Sodium" || param === "Potassium" || param === "Urée" || param === "Leucocytes" || param === "Polynucléaires neutrophiles" ) {
+        if (param === "Hémoglobine" || param === "TCA Patient/Témoin" || param === "ASAT" || param === "ALAT" || param === "Phosphatases alcalines" || param === "Gamma-GT" || param === "TP" || param === "INR" || param === "Sodium" || param === "Potassium" || param === "Urée" || param === "Leucocytes" || param === "Polynucléaires neutrophiles" || param === "Créatinine" ) {
             bioDeBaseText += bioDeBaseParams[param].name + " : " + formattedValue.trim(); // Do not add newline after 
             skipNewLine = true; // Set flag to skip newline after ASAT
         } else {
@@ -110,7 +118,7 @@ for (var param in bioDeBaseParams) {
             }
         }
     } else {
-        if (param === "Hémoglobine" || param === "ASAT" || param === "ALAT" || param === "Phosphatases alcalines" || param === "Gamma-GT" || param === "TP" || param === "INR" || param === "Sodium" || param === "Potassium" || param === "Urée" || param === "Leucocytes"  || param === "Polynucléaires neutrophiles" ) {
+        if (param === "Hémoglobine" || param === "TCA Patient/Témoin" || param === "ASAT" || param === "ALAT" || param === "Phosphatases alcalines" || param === "Gamma-GT" || param === "TP" || param === "INR" || param === "Sodium" || param === "Potassium" || param === "Urée" || param === "Leucocytes"  || param === "Polynucléaires neutrophiles" || param === "Créatinine" ) {
             bioDeBaseText += bioDeBaseParams[param].name + " : _____ " + bioDeBaseParams[param].unit; // Do not add newline after 
             skipNewLine = true; // Set flag to skip newline after ASAT
         } else {
@@ -129,18 +137,17 @@ for (var param in bioDeBaseParams) {
 
 
     // Créer une chaîne pour stocker le texte formaté du bilan phosphocalcique
-    var bilanPhosphocalciqueText = '';
+    var bilanPhosphocalciqueText = 'Bilan phosphocalcique : ';
 
     // Définition des paramètres pour le bilan phosphocalcique et leurs noms formatés avec les unités
     var bilanPhosphocalciqueParams = {
         "Calcium": { name: "Calcium", unit: "mmol/L" },
         "Ca total corrigé": { name: "Calcémie corrigée (albumine)", unit: "mmol/L" },
         "Phosphore": { name: "Phosphore", unit: "mmol/L" },
-        "25-OH Vitamine D": { name: "25-OH Vitamine D", unit: "nmol/L" },
+        "25-OHVitamineD": { name: "25-OH Vitamine D", unit: "nmol/L" },
         "PTH": { name: "PTH", unit: "ng/L" },
         "TSH": { name: "TSH", unit: "mUI/L" },
     };
-
 
     // Remplacer les valeurs dans le texte formaté du bilan phosphocalcique et les placer à leurs positions respectives
     for (var param in bilanPhosphocalciqueParams) {
@@ -148,14 +155,14 @@ for (var param in bioDeBaseParams) {
         var valueObject = findValue(variations, preprocessedText, bilanPhosphocalciqueParams[param].unit);
         if (valueObject) {
             var formattedValue = valueObject.operator + " " + valueObject.value + " " + (bilanPhosphocalciqueParams[param].unit === "%" ? "" : bilanPhosphocalciqueParams[param].unit);
-            bilanPhosphocalciqueText += bilanPhosphocalciqueParams[param].name + " : " + formattedValue.trim() + "\n";
+            bilanPhosphocalciqueText += bilanPhosphocalciqueParams[param].name + " : " + formattedValue.trim() + ", ";
         } else {
-            bilanPhosphocalciqueText += bilanPhosphocalciqueParams[param].name + " : _____ " + bilanPhosphocalciqueParams[param].unit + "\n";
+            bilanPhosphocalciqueText += bilanPhosphocalciqueParams[param].name + " : _____ " + bilanPhosphocalciqueParams[param].unit + ", ";
         }
     }
 
-    // Ajouter la ligne EPS à la fin du bilan
-    bilanPhosphocalciqueText += "EPS : _____\n";
+    // Ajouter la ligne EPS à la fin du bilan, sans virgule finale
+    bilanPhosphocalciqueText += "EPS : _____";
 
     // Afficher le texte formaté du bilan phosphocalcique dans la zone de sortie correspondante
     document.getElementById("bilanPhosphocalciqueText").value = bilanPhosphocalciqueText.trim();
@@ -165,28 +172,28 @@ for (var param in bioDeBaseParams) {
     var bilanAutoImmunParams = {
         "Rhumatoides": { name: "Facteurs Rhumatoides", unit: "" },
         "AntiPeptidesCycliquesCitrullinés": { name: "Anticorps Anti-CPP", unit: "" },
-        "AntiNucléaires(cellules HEp2)": { name: "Anti Nucléaires (cellules HEp2)", unit: "" },
+        "AntiNucléaires(cellules HEp2)": { name: "Anticorps Anti Nucléaires (cellules HEp2)", unit: "" },
         "ANCA": { name: "ANCAs", unit: "" },
     };
 
-    // Fonction pour formater le bilan auto-immun
+    // Fonction pour formater le bilan auto-immun avec des virgules
     function formatBilanAutoImmun(inputText) {
-        var formattedBilanAutoImmun = 'Bilan auto-immun:\n';
+        var formattedBilanAutoImmun = 'Bilan auto-immun : ';
         for (var param in bilanAutoImmunParams) {
             var pattern = new RegExp(param + "\\s*[:\\s]*\\s*([\\w\\s]+)", "i");
             var match = inputText.match(pattern);
             if (match) {
                 var value = match[1].trim();
-                // Vérifier si la valeur contient uniquement des chiffres
                 if (!/^\d+(\.\d+)?$/.test(value)) {
                     value = "_____";
                 }
-                formattedBilanAutoImmun += bilanAutoImmunParams[param].name + " : " + value + " " + bilanAutoImmunParams[param].unit + "\n";
+                formattedBilanAutoImmun += bilanAutoImmunParams[param].name + " : " + value + " " + bilanAutoImmunParams[param].unit + ", ";
             } else {
-                formattedBilanAutoImmun += bilanAutoImmunParams[param].name + " : _____ " + bilanAutoImmunParams[param].unit + "\n";
+                formattedBilanAutoImmun += bilanAutoImmunParams[param].name + " : _____ " + bilanAutoImmunParams[param].unit + ", ";
             }
         }
-        return formattedBilanAutoImmun;
+        // Supprimer la dernière virgule
+        return formattedBilanAutoImmun.slice(0, -2);
     }
 
     // Appel de la fonction pour formater le bilan auto-immun et stocker le texte formaté dans la variable correspondante
@@ -197,7 +204,7 @@ for (var param in bioDeBaseParams) {
     bilanAutoImmunTextarea.value = bilanAutoImmunText.trim();
 
     // Créer une chaîne pour stocker le texte formaté du bilan nutritionnel
-    var bilannutritionnelText = '';
+    var bilannutritionnelText = 'Bilan nutritionnel : ';
 
     // Définition des paramètres pour le bilan nutritionnel et leurs noms formatés avec les unités
     var bilannutritionnelParams = {
@@ -214,14 +221,29 @@ for (var param in bioDeBaseParams) {
         var valueObject = findValue(variations, preprocessedText, bilannutritionnelParams[param].unit);
         if (valueObject) {
             var formattedValue = valueObject.operator + " " + valueObject.value + " " + (bilannutritionnelParams[param].unit === "%" ? "" : bilannutritionnelParams[param].unit);
-            bilannutritionnelText += bilannutritionnelParams[param].name + " : " + formattedValue.trim() + "\n";
+            bilannutritionnelText += bilannutritionnelParams[param].name + " : " + formattedValue.trim() + ", ";
         } else {
-            bilannutritionnelText += bilannutritionnelParams[param].name + " : _____ " + bilannutritionnelParams[param].unit + "\n";
+            bilannutritionnelText += bilannutritionnelParams[param].name + " : _____ " + bilannutritionnelParams[param].unit + ", ";
         }
     }
 
+    // Supprimer la dernière virgule
+    bilannutritionnelText = bilannutritionnelText.slice(0, -2);
+
     // Afficher le texte formaté du bilan nutritionnel dans la zone de sortie correspondante
     document.getElementById("bilannutritionnelText").value = bilannutritionnelText.trim();
+
+    // Appliquer la fonction de suppression des sauts de ligne sur chaque section
+    bioDeBaseText = removeExtraNewlines(bioDeBaseText.trim());
+    bilanPhosphocalciqueText = removeExtraNewlines(bilanPhosphocalciqueText.trim());
+    bilanAutoImmunText = removeExtraNewlines(bilanAutoImmunText.trim());
+    bilannutritionnelText = removeExtraNewlines(bilannutritionnelText.trim());
+
+    // Combiner les sections en ajoutant deux sauts de ligne entre elles
+    var finalText = bioDeBaseText + '\n' + bilanPhosphocalciqueText + '\n' + bilanAutoImmunText + '\n' + bilannutritionnelText;
+
+    // Appliquer removeExtraNewlines au texte final pour s'assurer qu'il n'y ait pas plus de deux sauts de ligne consécutifs
+    document.getElementById("finalText").value = removeExtraNewlines(finalText.trim());
 
   });
   
